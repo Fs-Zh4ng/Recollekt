2805589dc5da5d7cc446ce963258b87c8233b489
Fs-Zh4ng <ferdinandsz08@gmail.com>
2025-06-13 21:05:02 -0400

    pray

--- Recollekt/app/index.tsx
+++ Recollekt/app/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Redirect } from 'expo-router';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Recollekt!</Text>
-    </View>
+    <Redirect href="/login/login" />
   );
 }
 
