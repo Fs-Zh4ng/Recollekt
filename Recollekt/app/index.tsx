e503878aff481483c4a476e3bfaf7f451a3edb15
Fs-Zh4ng <ferdinandsz08@gmail.com>
2025-06-13 21:13:25 -0400

    idek anymore

--- Recollekt/app/index.tsx
+++ Recollekt/app/index.tsx
@@ -3,21 +3,8 @@ import { View, Text, StyleSheet } from 'react-native';
 import { Redirect } from 'expo-router';
 
 export default function Home() {
+    console.log('Home component rendered');
   return (
     <Redirect href="/login/login" />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-});
